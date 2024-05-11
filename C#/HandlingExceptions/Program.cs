96fba2b768160e16f25d02069a88165fa366baab
Thiru-4651 <166098701+Thiru-4651@users.noreply.github.com>
2024-05-11 23:57:28 +0530

    Add files via upload

---
+++ C#/HandlingExceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace SwitchAssignment;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            System.Console.Write("Enter the first number: ");
+            int number1 = int.Parse(Console.ReadLine());
+            System.Console.Write("Enter the second number: ");
+            int number2 = int.Parse(Console.ReadLine());
+            System.Console.WriteLine(number1 / number2);
+        }
+        catch (FormatException)
+        {
+            System.Console.WriteLine("Input was not in correct form");
+            //throw;
+        }
+        
+    }
+}
---
+++ C#/SwitchAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace SwitchAssignment;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.Write("Enter the number1: ");
+        int number1 = int.Parse(Console.ReadLine());
+
+        System.Console.Write("Enter the number2: ");
+        int number2 = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("Press 1 to add two number\nPress 2 to subtract two number\nPress 3 to multiply two number\nPress 4 to divide two number");
+        int operation=int.Parse(Console.ReadLine());
+
+        switch (operation)
+        {
+            case 1:
+                {
+                    System.Console.WriteLine(number1+number2);
+                    break;
+                }
+            case 2:
+                {
+                    System.Console.WriteLine(number1-number2);
+                    break;
+                }
+            case 3:
+                {
+                    System.Console.WriteLine(number1*number2);
+                    break;
+                }
+            case 4:
+                {
+                    System.Console.WriteLine(number1/number2);
+                    break;
+                }
+
+        }
+    }
+}
